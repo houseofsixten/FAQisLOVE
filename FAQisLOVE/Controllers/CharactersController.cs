4aaa7b2a812dfd97a49c0c5513f0884e442612c2
Adrian Ferrer <guuonearth@yahoo.com>
2023-10-13 17:27:56 -0700

    Initial commit 2

---
+++ FAQisLOVE/Controllers/CharactersController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class CharactersController : Controller
+    {
+        private readonly string[] characters = { "Miko", "Sora", "Suisei", "Haato", "Pekora", "Amelia" };
+
+        public IActionResult Miko()
+        {
+            ViewBag.Characters = characters;
+            ViewBag.CharName = "Miko";
+            return View();
+        }
+
+        public IActionResult Sora()
+        {
+            ViewBag.Characters = characters;
+            ViewBag.CharName = "Sora";
+            ViewBag.ModelImages = new string[] { "sora_model_01.png", "sora_model_02.png", "sora_model_03.png" };
+            return View();
+        }
+
+        public IActionResult Suisei()
+        {
+            ViewBag.Characters = characters;
+            ViewBag.CharName = "Suisei";
+            return View();
+        }
+
+        public IActionResult Haato()
+        {
+            ViewBag.Characters = characters;
+            ViewBag.CharName = "Haato";
+            return View();
+        }
+
+        public IActionResult Pekora()
+        {
+            ViewBag.Characters = characters;
+            ViewBag.CharName = "Pekora";
+            return View();
+        }
+
+        public IActionResult Amelia()
+        {
+            ViewBag.Characters = characters;
+            ViewBag.CharName = "Amelia";
+            return View();
+        }
+    }
+}
